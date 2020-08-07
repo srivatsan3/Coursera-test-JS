c8a6755f7493aadbefff383cef29f826541ccce4
srivatsan3 <68266216+srivatsan3@users.noreply.github.com>
2020-08-07 11:23:53 +0530

    Add files via upload

---
+++ Mod5_Assignment/src/public/sign-up/sign-up-data.service.js
@@ -0,0 +1,20 @@
+(function(){
+
+	'use strict';
+
+	angular.module('public')
+	.service('SignUpDataService',SignUpDataService);
+
+	function SignUpDataService(){
+		var service = this;
+		var userPref;
+
+		service.setUserPref = function(userPref){
+			service.userPref = userPref;
+		}
+
+		service.getUserPref = function(){
+			return service.userPref;
+		}
+	}
+})();
